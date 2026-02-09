86e45db16cafb8185cb3887797d0e798dcf2b1da
Negrut Cosmin <cosminnegrut776@gmail.com>
2026-02-09 23:54:31 +0200

    .

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-  
-  
-  
-let btnUp=document.getElementById("up");
+
+const scrollBtn = document.querySelector(".scroll-btn");
+window.onscroll = function() {
+
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
